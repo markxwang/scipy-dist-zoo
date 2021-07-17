0b9d31eb98a31de7d195244d0ef1a7719350443a
Xin Wang <xin.wang@ihsmarkit.com>
2021-07-17 15:46:24 +0100

    refactor plot function

--- app.py
+++ app.py
@@ -37,64 +37,74 @@ def ls_params(distribution):
     return p
 
 
+def gen_x(dist, is_continuous, ql=0.005, qr=0.995, n=100):
+    if is_continuous:
+        x = np.linspace(dist.ppf(ql), dist.ppf(qr), n)
+    else:
+        x = np.arange(dist.ppf(ql), dist.ppf(qr))
+    return x
+
+
+def gen_y(dist, x, is_continuous, is_cdf):
+    if is_cdf:
+        y = dist.cdf(x)
+    else:
+        if is_continuous:
+            y = dist.pdf(x)
+        else:
+            y = dist.pmf(x)
+    return y
+
+
+def gen_fig(df, is_continuous):
+    fig = alt.Chart(df)
+    yaxis = alt.Y("y", axis=alt.Axis(title=""))
+
+    if is_continuous:
+        fig = fig.mark_line().encode(alt.X("x", axis=alt.Axis(title="")), yaxis)
+    else:
+        fig = fig.mark_bar().encode(alt.X("x", axis=alt.Axis(tickMinStep=1, title="")), yaxis)
+    return fig
+
+
 continuous_dists = get_continuous_dist()
 discrete_dists = get_discrete_dist()
 
-
 st.sidebar.write("## Select distribution")
 dist_cat = st.sidebar.radio("", ["continuous", "discrete"])
+continous_flag = dist_cat == "continuous"
 
-param_val_default = dict(loc=0, scale=1)
+param_default = dict(loc=0, scale=1, p=0.5)
 
-if dist_cat == "continuous":
+if continous_flag:
     dist_list = continuous_dists
-elif dist_cat == "discrete":
+else:
     dist_list = discrete_dists
 
 dist_name = st.sidebar.selectbox("", dist_list)
 dist = get_dist(dist_name)
 
+scipy_link = f"https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.{dist_name}.html"
+st.sidebar.markdown(f"[`scipy.stats.{dist_name}`]({scipy_link})")
 
-st.sidebar.write("## Set distribution parameters")
+st.sidebar.write("## Set parameters")
 
-param_names = ls_params(dist)
-param_dict = {}
-for name in param_names:
-    param_dict[name] = st.sidebar.number_input(
-        f"{dist_name} [{name}]", value=float(param_val_default.get(name, 1)), step=0.1
-    )
+param_dict = {
+    p: st.sidebar.number_input(f"{dist_name} [{p}]", value=float(param_default.get(p, 1)), step=0.1)
+    for p in ls_params(dist)
+}
 
 
 dist = dist(**param_dict)
 
-
-st.write("## Density Plot")
 cdf_flag = st.sidebar.checkbox("CDF")
 
 st.sidebar.info('This app is developed by [Mark (Xin) Wang](mailto:wxgter@gmail.com")')
 
-if dist_cat == "continuous":
-    x = np.linspace(dist.ppf(0.005), dist.ppf(0.995), 100)
-    if cdf_flag:
-        y = dist.cdf(x)
-    else:
-        y = dist.pdf(x)
 
-    df_plot = pd.DataFrame({"x": x, "density": y})
-    fig = alt.Chart(df_plot).mark_line().encode(x="x", y="density")
-elif dist_cat == "discrete":
-    x = np.arange(dist.ppf(0.005), dist.ppf(0.995))
-    if cdf_flag:
-        y = dist.cdf(x)
-    else:
-        y = dist.pmf(x)
+x = gen_x(dist, continous_flag)
+y = gen_y(dist, x, continous_flag, cdf_flag)
+df = pd.DataFrame({"x": x, "y": y})
 
-    df_plot = pd.DataFrame({"x": x, "density": y})
-    fig = alt.Chart(df_plot).mark_bar().encode(alt.X("x", axis=alt.Axis(tickMinStep=1)), alt.Y("density"))
-
-st.altair_chart(fig, use_container_width=True)
-
-
-scipy_link = f"https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.{dist_name}.html"
-
-st.markdown(f"[🚀 {dist_name} doc]({scipy_link})")
+st.write("## Density Plot")
+st.altair_chart(gen_fig(df, continous_flag), use_container_width=True)
